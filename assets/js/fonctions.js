f50c512e89867bde0223cde010cf1c9934bd2369
dynamo63 <smoulongui@gmail.com>
2020-05-23 15:06:36 +0100

    Controle des informations du formulaire

--- assets/js/fonctions.js
+++ assets/js/fonctions.js
@@ -1,3 +1,18 @@
+const emailRegex = /.+@.+\..+/;
+
 /**
- * Fonctions de travail
+ * Cache le message du contenu <div class="message">...</div>
+ * @param {Element} messageElement 
  */
+ const hideMessage = messageElement => {
+     messageElement.classList.remove('message-visible')
+ }
+
+/**
+ * Retourne true si l'email est valide
+ * @param {String} email - email value
+ * @returns {Boolean}
+ */
+ const emailValidation = email => {
+    return email && emailRegex.test(email) ? true : false;
+ }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
-/**
- * Code de gestion du formulaire
- */
+const form = document.forms[0];
+const emailInput = form.email;
+const passwordInput = form.pass;
+const notificationElt = document.querySelector('.message');   
+
+// console.log(form.email)
+
+form.onsubmit = e => {
+    e.preventDefault();
+    notificationElt.classList.add('message-visible');
+    let password = passwordInput.value;
+    
+    if (emailValidation(emailInput.value) && password.length >= 8) {
+        form.style.backgroundColor = '#a7ff3342';
+        hideMessage(notificationElt);
+    } else {
+        notificationElt.textContent = 'Email incorrecte ou Mot de Passe trop court';
+    }
+    
+}
+
+emailInput.onfocus = () => {
+    hideMessage(notificationElt);
+}
+
+passwordInput.onfocus = () => {
+    hideMessage(notificationElt);
+}
